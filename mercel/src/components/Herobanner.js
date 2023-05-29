5130d14e2853bc858fc231b760455bd213258ac4
vardhanreddy27 <2527webmaster@gmail.com>
2023-05-29 15:20:55 +0530

    allsol

--- mercel/src/components/Herobanner.js
+++ mercel/src/components/Herobanner.js
@@ -4,12 +4,14 @@ function Herobanner() {
   return (
     <div>
       <div className="row herobannerbg">
-        <h1 className="herobanner">
-          Latest
-          <br />
-          Trending Offers
-        </h1>
-        <button className="btn shopnow">shop now</button>
+        <div className="">
+          <h1 className="herobanner">
+            Latest
+            <br />
+            Trending Offers
+          </h1>
+          <button className="btn shopnow">shop now</button>
+        </div>
       </div>
     </div>
   );
