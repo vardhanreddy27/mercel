5e2fdf95c6fdc020c1723099a8558a28ef7f3923
vardhanreddy27 <2527webmaster@gmail.com>
2023-06-11 16:28:11 +0530

    half profile pagesppllewwzzll..ssttlllss-=allsllookkffppoojjpplll

--- mercel/src/components/Herobanner.js
+++ mercel/src/components/Herobanner.js
@@ -3,16 +3,17 @@ function Herobanner() {
   return (
     <>
       <div className="row herobannerbg">
-        <div className="col-8">
+        <div className="col-4">
           <h1 className="ps-3 pt-4 font">
             Latest
             <br />
             Trending Offers
           </h1>
+          <button className="btn shopnow">shop now</button>
         </div>
-        <div className="col-4">img herobannerbg </div>
-
-        <button className="btn shopnow">shop now</button>
+        <div className="col-8 bimg">
+          <img src="./banner.png" alt="banner img " />
+        </div>
       </div>
       <div className="offer2">
         <div className="">great sale | great sale |</div>
@@ -21,7 +22,7 @@ function Herobanner() {
       </div>
       <div className="fluid-container gradient1 text-center">
         <div className="d-flex">
-          <div className="card">
+          <div className="card shadow">
             <div className="card-header text-center">Featured</div>
             <img
               className=""
@@ -33,46 +34,50 @@ function Herobanner() {
               <p className="font-weight-bold">upto 60% off</p>
             </div>
           </div>
-          <div className="card">
-            <div className="latestbadge card-header text-center">Latest</div>
+          <div className="card shadow">
+            <div className="card-header text-center">Best Sellers</div>
             <img
               className=""
-              src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Electronics/Clearance/Clearance_store_Desktop_CC_2x._SY608_CB628315133_.jpg"
+              src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/AmazonBrands/Bikram/Bikram1/cycle_379._SY232_CB592080532_.jpg"
               alt="Card cap"
             />
             <div className="card-body">
-              <p className="card-text">Electronics & More</p>
-              <p className="font-weight-bold">upto 60% off</p>
+              <p className="card-text">Adults & Kids Cycles</p>
+              <p className="font-weight-bold">upto 20% off</p>
             </div>
           </div>
         </div>
         <div className="d-flex">
-          <div className="card">
-            <div className="card-header text-center">Featured</div>
+          <div className="card shadow">
+            <div className="card-header text-center">Top Rated</div>
             <img
               className=""
-              src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Electronics/Clearance/Clearance_store_Desktop_CC_2x._SY608_CB628315133_.jpg"
+              src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Fashion/Gateway/BAU/BTF-Refresh/May/PC-PFF/PFF-4-372-232._SY232_CB636055991_.jpg"
               alt="Card cap"
             />
             <div className="card-body">
-              <p className="card-text">Electronics & More</p>
-              <p className="font-weight-bold">upto 60% off</p>
+              <p className="card-text">Fashion & Clothing</p>
+              <p className="font-weight-bold">Clearance Sale</p>
             </div>
           </div>
-          <div className="card">
-            <div className="card-header text-center">Featured</div>
+          <div className="card shadow">
+            <div className="card-header text-center">Latest</div>
             <img
               className=""
-              src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Electronics/Clearance/Clearance_store_Desktop_CC_2x._SY608_CB628315133_.jpg"
+              src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Fashion/Gateway/BAU/BTF-Refresh/May/PC-PFF/PFF-3-372-232._SY232_CB636055991_.jpg"
               alt="Card cap"
             />
             <div className="card-body">
-              <p className="card-text">Electronics & More</p>
-              <p className="font-weight-bold">upto 60% off</p>
+              <p className="card-text ps-1 pe-1">Backpacks & Bags</p>
+              <p className="font-weight-bold">upto 40% off</p>
             </div>
           </div>
         </div>
       </div>
+      <br />
+
+      <br />
+      <br />
       {/* <div className="container">
         <div className="row textcenter mt-4">
           <div className="col-5 gradient">
