7db81dad586e44c597a844f150d775bf621a45de
vardhanreddy27 <2527webmaster@gmail.com>
2023-06-15 00:28:23 +0530

    half profile pagesppllewwzzll..ssttlllss-=allsllookkffppoojjpplll[[ll;;//ssppsssssddlldd

--- mercel/src/pages/Login.js
+++ mercel/src/pages/Login.js
@@ -1,31 +1,37 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { FaApple } from "react-icons/fa";
 
 function Login() {
   return (
     <>
       <div className="login-image-container">
-        <img src="./logo.svg" alt="" className="logo-image" />
+        <h1 className="loginfont">VINKLE </h1>
       </div>
-      <div className="container">
-        <div className="row text-center dashed-line-center mt-4">
-          <h1 className="loginfont">Welcome to vinkle</h1>
-          <p className="loginfont">connect with our community</p>
-          <div className="mywidth">
-            <button
-              type="button"
-              className="btn sbtn btn-outline-secondary btn-lg btn-block"
-            >
-              google
-            </button>
-          </div>
-          <hr className="dashed-line" />
-          <div className="mywidth">
-            <button
-              type="button"
-              className="btn sbtn btn-outline-secondary btn-lg btn-block"
-            >
-              apple
-            </button>
+      <div className="card logincard logincard1 ">
+        <div className="card-body bodylogin">
+          <div className="row p-4">
+            <div className="col">
+              <h1>
+                Welcome to <span className="logotxt">VINKLE</span>
+              </h1>
+              <p>connect with our community</p>
+              <br />
+              <button
+                type="button"
+                className="btn social googlebtn border btn-lg btn-block"
+              >
+                <FcGoogle className="me-3" />
+                Login with Google
+              </button>
+              <hr className="dashed-line" />
+              <button
+                type="button"
+                className="btn social applebtn border btn-lg btn-block"
+              >
+                <FaApple className="me-3" /> Login with Apple
+              </button>
+            </div>
           </div>
         </div>
       </div>
