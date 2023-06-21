e383497bde7f311b2a416563ca1ab1e97ff24bce
vardhanreddy27 <2527webmaster@gmail.com>
2023-06-22 00:31:19 +0530

    now

--- mercel/api/shops.js
+++ mercel/api/shops.js
@@ -1,21 +1,16 @@
 const { MongoClient } = require("mongodb");
-
 const MONGODB_URI =
   "mongodb+srv://vishnu:rrr123@cluster0.fczkwxs.mongodb.net/vinkle?retryWrites=true&w=majority";
 const DATABASE_NAME = "vinkle";
 const COLLECTION_NAME = "Shops";
-const { name, image, address,category } = req.body;
 
 async function connectToDatabase() {
   const client = new MongoClient(MONGODB_URI);
-
   try {
     await client.connect();
     console.log("Connected to the database");
-
     const database = client.db(DATABASE_NAME);
     const collection = database.collection(COLLECTION_NAME);
-
     const data = await collection.find().toArray();
     return data;
   } catch (error) {
@@ -26,7 +21,6 @@ async function connectToDatabase() {
     console.log("Disconnected from the database");
   }
 }
-
 module.exports = async (req, res) => {
   try {
     const data = await connectToDatabase();
