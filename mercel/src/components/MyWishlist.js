e2cc89f681625fd1bee12d802e50e651cd494790
vardhanreddy27 <2527webmaster@gmail.com>
2023-07-09 17:24:39 +0530

    remolslsddslljjs

--- mercel/src/components/MyWishlist.js
+++ mercel/src/components/MyWishlist.js
@@ -11,27 +11,29 @@ import 'react-toastify/dist/ReactToastify.css';
 function MyWishlist() {
   const handleAddToCart = async (product) => {
     try {
-      // Remove the product from the wishlist
-      await handleToggleWishlist(product);
-  
       const payload = {
         product,
         userEmail: user.email,
       };
   
-      // Make a POST request to the serverless function
-      const response = await axios.post(
+      // Make a POST request to add the product to the cart
+      const addToCartResponse = await axios.post(
         'https://mercel.vercel.app/api/addToCart',
         payload
       );
   
-      // Handle the response if needed
+      // Remove the product from the wishlist
+      await handleToggleWishlist(product);
   
-      // Example: Display a success message
+      // Handle the responses if needed
+  
+      // Example: Display success messages
       toast.success('Product added to cart!');
+      toast.success('Product removed from wishlist!');
     } catch (error) {
       console.error('Failed to add product to cart:', error);
       toast.error('Failed to add product to cart');
+      toast.error('Failed to remove product from wishlist');
     }
   };
   const wishlistItems = useSelector((state) => state.wishlist);
