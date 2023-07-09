96dc5f9310d614dba789b7b1df6c828be1280572
vardhanreddy27 <2527webmaster@gmail.com>
2023-07-09 12:54:07 +0530

    remol

--- mercel/src/pages/Cart.js
+++ mercel/src/pages/Cart.js
@@ -27,7 +27,9 @@ const Cart = () => {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
-          },
+            'Cache-Control': 'no-cache, no-store, must-revalidate',
+            Pragma: 'no-cache',
+            Expires: '0',          },
         });
 
         if (response.ok) {
