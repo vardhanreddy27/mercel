334c3c94f2ac23613fe721525ad0065990f423f8
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2023-08-20 21:59:26 +0530

    shps pages..llp

--- mercel/src/components/Back.js
+++ mercel/src/components/Back.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import { IoArrowBackSharp } from "react-icons/io5";
-
-function Back() {
-    
-    const navigate = useNavigate();
-
-  return (
-    <>
-         <div className="mt-4 mb-4 ms-3 d-flex" onClick={() => navigate(-1)}>
-    <IoArrowBackSharp size={20}/> 
-      </div>
+import { IoChevronBackOutline } from "react-icons/io5";
+import { Button, Dialog } from "@mui/material";
+function Back() { 
+const navigate = useNavigate();
+return (
+    <>      
+      <Button className="backbtn">
+            <IoChevronBackOutline size={30} onClick={() => navigate(-1)} className="backbtnicon" />
+      </Button>
     </>
   )
 }
